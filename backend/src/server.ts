ed4620e07ef06ea661a2f5b9cbcbb8a97463ec3c
FeketeMiklosBenjamin <fekete.miklos.benjamin@students.jedlik.eu>
2025-04-14 12:41:50 +0200

    get elkészítése

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import { ItemRoutes } from './routes/item.routes';
 import App from './app';
 import { AuthRoutes } from './routes/auth.routes';
+import { CategoryRoutes } from './routes/category.routes';
 
 new App([
     new AuthRoutes(),
-    new ItemRoutes()
+    new ItemRoutes(),
+    new CategoryRoutes()
 ]);
-
