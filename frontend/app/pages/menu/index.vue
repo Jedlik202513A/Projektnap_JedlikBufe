620c2b273fcabcc805ac9f64d77371bde1e31f01
pallanghunor <pallang.hunor@students.jedlik.eu>
2025-04-14 13:34:06 +0200

    feat: add Google Fonts integration and update styles for categories and menu layout

--- frontend/app/pages/menu/index.vue
+++ frontend/app/pages/menu/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { Category } from '~/types/Category';
 import type { Item } from '~/types/Item';
 definePageMeta({
     layout: 'mobil',
@@ -6,61 +7,56 @@ definePageMeta({
 
 const { getMenu } = useMenuApi()
 
-const loadMenu = async () => {
-    try {
-        const response = await getMenu()
-        console.log(response)
-    } catch (error) {
-        console.error("Error loading menu:", error)
-    }
-}
+// const loadMenu = async () => {
+//     try {
+//         const response = await getMenu()
+//         console.log(response)
+//     } catch (error) {
+//         console.error("Error loading menu:", error)
+//     }
+// }
 
-const MenuItems: Item[] = [
+// onMounted(() => {
+//     loadMenu()
+// })
+
+const categories = ref<Category[]>([
     {
-        id: "0",
-        name: "Mr. Big szendvics",
-        price: 1000,
-        stock: 12,
-        category: {
-            id: "0",
-            name: "Szendvics"
-        }
+        id: "1",
+        name: 'Pizza'
     },
     {
-        id: '1',
-        name: 'Fehér zsemlés szendvics',
-        category: {
-             id:"0",
-             name: "Szendvics"
-        },
-        price: 350,
-        stock: 20
+        id: "2",
+        name: 'Tészta'
     },
     {
-        id: '2',
-        name: 'Classic Burger',
-        category: {
-            id:"1",
-            name:"Hamburger"
-        },
-        price: 800,
-        stock: 4
+        id: "3",
+        name: 'Saláta'
+    },
+    {
+        id: "4",
+        name: 'Desszert'
+    },
+    {
+        id: "5",
+        name: 'Ital'
+    },
+    {
+        id: "6",
+        name: 'Kávé'
     }
-]
+])
 </script>
 
 <template>
     <div class="p-5">
-        <h1 class="text-2xl font-bold">Szia Lajos!</h1>
-        <p>Rendelj valami finomat!</p>
+        <h1 class="text-3xl font-semibold">Szia Lajos!</h1>
+        <p class="text-gray-500 font-medium">Rendelj valami finomat!</p>
     </div>
-    <div class="grid grid-cols-1 gap-4 rounded-3xl p-4 md:grid-rows-2 lg:grid-rows-3">
-        <FoodCard v-for="item in MenuItems" :key="item.id" :item="item"/>
-        <Cart/>
+    <div class="flex flex-col gap-2.5">
+        <CategoriesCard v-for="category in categories" :key="category.id" :category="category" />
     </div>
 </template>
-
-
 
 <style scoped>
 
