a124139f6ad7fc2e35dc1ed8ac228bcaec3d59b6
Akos Kovacs <kovacs.akos@students.jedlik.eu>
2025-04-14 09:30:17 +0200

    cart page start

---
+++ frontend/app/pages/cart/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+      
+</script>
+
+<template>
+    <div>
+        <h1>Cart</h1>
+        <p>Cart page content goes here.</p>
+        <div>
+            <NuxtLink to="/">Go to Home</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
