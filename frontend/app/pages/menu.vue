74b538bc9c4982a6c8be32ecf6a97ae95b6316db
norbert-gemes <gemes.norbert@students.jedlik.eu>
2025-04-14 09:26:31 +0200

    Menu oldal létrehozva

---
+++ frontend/app/pages/menu.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const MenuItems: items()=>{
+    
+}
+</script>
+
+<template>
+    <div>
+        <FoodCard v-for="">
+
+        </FoodCard>
+    </div>
+</template>
+
+
+
+<style scoped>
+</style>
