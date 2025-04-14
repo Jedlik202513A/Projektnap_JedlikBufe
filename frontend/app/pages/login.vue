b1069db8e24ba169f5506f650143d48f8b1acd2d
DomiNate2 <jdomi04@gmail.com>
2025-04-14 10:06:01 +0200

    Bejelentkezés kész

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'blank'
+    layout: 'blank'
 })
 
 
 </script>
 
 <template>
-    <div class="max-w-4xl h-96 border mx-auto border-white flex justify-center items-center">
-        <input type="text" placeholder="Felhasználó" class="max-w-150 h-15">
+    <div class="h-screen flex items-center justify-center">
+        <form @submit.prevent=""
+            class="w-1/4 max-w-5xl flex flex-col items-center gap-5 border mx-auto border-white rounded-2xl p-10 bg-neutral-200/20">
+            <input type="text" placeholder="Felhasználónév"
+                class="w-full h-10 border bg-amber-50 text-amber-950 rounded-2xl p-4">
+            <input type="password" placeholder="Jelszó"
+                class="w-full h-10 border bg-amber-50 text-amber-950 rounded-2xl p-4">
+            <button type="form" class="bg-sky-500 text-stone-950 p-3 rounded-2xl">Bejelentkezés</button>
+        </form>
     </div>
 </template>
 
 
-<style scoped>
-</style>
+<style scoped></style>
