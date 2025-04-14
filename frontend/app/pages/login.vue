1115315a0be381e96f9247d495341c89f453bf45
DomiNate2 <jdomi04@gmail.com>
2025-04-14 09:25:31 +0200

    Login megkezdése

---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'blank'
+})
+
+
+</script>
+
+<template>
+    <div class="max-w-4xl h-96 border mx-auto border-white flex justify-center items-center">
+        <input type="text" placeholder="Felhasználó" class="max-w-150 h-15">
+    </div>
+</template>
+
+
+<style scoped>
+</style>
