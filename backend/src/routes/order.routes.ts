2b67345fa884f922a033f8603ba3677d64724c5b
boka matyas <boka.matyas@students.jedlik.eu>
2025-04-14 13:41:43 +0200

    Fuck me harder

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -6,6 +6,7 @@ export class OrderRoutes {
     
     constructor() {
         this.router.get('/api/orders', OrderController.getAll)        
+        this.router.get('/api/orders/:id', OrderController.getOrderById)        
         this.router.post('/api/orders', OrderController.newOrder)        
     }
 }
